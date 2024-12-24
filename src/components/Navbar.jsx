29103ec4132139f6e1a4d7841bc1ae0316565066
Tony <tony.saintamour@gmail.com>
2024-12-24 18:14:03 -0500

    dashboard

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,6 +19,7 @@ export default function Navbar() {
     const navItems = [
         { path: '/', label: 'Home' },
         { path: '/blog', label: 'Blog' },
+        { path: '/dashboard', label: 'Dashboard' }
     ];
 
     return (
